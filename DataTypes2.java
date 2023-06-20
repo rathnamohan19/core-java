3a1f51dc9ece15825f93d8d47c63c059601e9b4b
RathnaBMohan <129740896+RathnaBMohan@users.noreply.github.com>
2023-06-20 23:26:18 +0530

    Data Types 2

--- DataTypes2.java
+++ DataTypes2.java
@@ -5,13 +5,119 @@ public class DataTypes2{
 		byte totalSubjects = 8;
 		byte attendance = 40;
 		byte totalShops = 8;
+		byte age = 40;
+		byte totalNumberOfMonths = 12;
+		byte roomTemperature = 30;
+		byte totalPrice = 80;
+		byte heightOfPerson = 7;
 		
 		System.out.println(totalStudents);
 		System.out.println(totalClass);
 		System.out.println(totalSubjects);
 		System.out.println(attendance);
 		System.out.println(totalShops);
+		System.out.println(age);
+		System.out.println(totalNumberOfMonths);
+		System.out.println(roomTemperature);
+		System.out.println(totalPrice);
+		System.out.println(heightOfPerson);
 		
-		short 
+		short totalMarks = 1000;
+		short totalUnits = 2000;
+		short pgRent = 6500;
+		short costOfGold = 6450;
+		short totalDistance = 5000;
+		short totalNumberOfStates = 4000;
+		short totalAccountBalance = 6000;
+		short totalCost = 8000;
+		short totalBooksInShelf = 4500;
+		short totalRacks = 1000;
+		
+		System.out.println(totalMarks);
+        System.out.println(totalUnits);
+		System.out.println(pgRent);
+		System.out.println(costOfGold);
+		System.out.println(totalDistance);
+		System.out.println(totalNumberOfStates);
+		System.out.println(totalAccountBalance);
+		System.out.println(totalCost);
+		System.out.println(totalBooksInShelf);
+		System.out.println(totalRacks);
+		
+		int numberOfCars = 100000;
+		int carPrice = 200000;
+		int salary = 100000;
+		int laptopPrice = 60000;
+		int housePrice = 500000;
+		int numberOfStars = 1000000;
+		int numberOfdays = 600000;
+		
+		System.out.println(numberOfCars);
+		System.out.println(carPrice);
+		System.out.println(salary);
+		System.out.println(laptopPrice);
+		System.out.println(housePrice);
+		System.out.println(numberOfStars);
+		System.out.println(numberOfdays);
+		
+		long phoneNumber = 9481553968l;
+		long aadharNumber = 5678954678l;
+		long panNumber = 45678965434l;
+		long id = 345678987l;
+		long usn = 3456789876l;
+		long pinCode = 572212l;
+		long accountNumber = 876512345l;
+		
+		System.out.println(phoneNumber);
+		System.out.println(aadharNumber);
+		System.out.println(panNumber);
+		System.out.println(id);
+		System.out.println(usn);
+		System.out.println(pinCode);
+		System.out.println(accountNumber);
+		
+		char grade = 'a';
+		char section = 'c';
+		char gender = 'f';
+		char result = 'p';
+		char platform = 'g';
+		
+		System.out.println(grade);
+		System.out.println(section);
+		System.out.println(gender);
+		System.out.println(result);
+		System.out.println(platform);
+		
+		float percentage = 98.6f;
+		float gravity = 4.6f;
+		float velocity = 5.67f;
+		float speed = 4.0f;
+		float rotationSpeed = 8.9f;
+		
+		System.out.println(percentage);
+		System.out.println(gravity);
+		System.out.println(velocity);
+		System.out.println(speed);
+		System.out.println(rotationSpeed);
+		
+		double rotationSpeedOfPlanet = 45.987654d;
+		double distanceFromEarth = 7765.9876543d;
+		
+		System.out.println(rotationSpeedOfPlanet);
+		System.out.println(distanceFromEarth);
+		
+		boolean isStudent = true;
+		boolean isPass = false;
+		boolean isFemale = false;
+		boolean sundayIsHoliday = true;
+		boolean isNull = false;
+		
+		System.out.println(isStudent);
+		System.out.println(isPass);
+		System.out.println(isFemale);
+		System.out.println(sundayIsHoliday);
+		System.out.println(isNull);
+		
+		
 	}
 }
