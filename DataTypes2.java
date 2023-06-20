3650526a8085e373f77411b62cec1f6176cc419c
RathnaBMohan <129740896+RathnaBMohan@users.noreply.github.com>
2023-06-20 12:30:38 +0530

    data types example

---
+++ DataTypes2.java
@@ -0,0 +1,17 @@
+public class DataTypes2{
+	public static void main(String[] args ){
+		byte totalStudents = 30;
+		byte totalClass = 40;
+		byte totalSubjects = 8;
+		byte attendance = 40;
+		byte totalShops = 8;
+		
+		System.out.println(totalStudents);
+		System.out.println(totalClass);
+		System.out.println(totalSubjects);
+		System.out.println(attendance);
+		System.out.println(totalShops);
+		
+		short 
+	}
+}
