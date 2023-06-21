0c791cc0d1192c00280f00e8ccf961acfbe0f7da
RathnaBMohan <129740896+RathnaBMohan@users.noreply.github.com>
2023-06-21 11:07:13 +0530

    Variables2 example

---
+++ Variables2.java
@@ -0,0 +1,75 @@
+public class Variables2{
+	public static void main(String[] args){
+		String nameOfFaculty = "abhishek ";
+		System.out.println(nameOfFaculty);
+	
+	
+		String picnicSpot = "park";
+		picnicSpot = "temple";
+		System.out.println(picnicSpot);
+		
+		char grade = 'A';
+		System.out.println(grade);
+		
+		byte age = 80;
+		System.out.println(age);
+		
+		boolean isNumber = true;
+		isNumber = false;
+		System.out.println(isNumber);
+		
+		int std = 6;
+		std = 7;
+		System.out.println(std);
+		
+		float percentage = 86.7f;
+		System.out.println(percentage);
+		
+		double gravity = 67.67899d;
+		System.out.println(gravity);
+		
+		long phoneNumber = 9481553968l;
+		System.out.println(phoneNumber);
+		
+		char gender = 'f';
+		gender = 'm';
+		System.out.println(gender);
+		
+		long adharNumber = 456789987l;
+		adharNumber = 4567898768l;
+		System.out.println(adharNumber);
+		
+		char vowels = 'a';
+		vowels= 'e';
+		System.out.println(vowels);
+		
+		String name = "Rathna";
+		name = "pavan";
+		System.out.println(name);
+		
+		int speedOfVehicle = 300;
+		speedOfVehicle = 200;
+		System.out.println(speedOfVehicle);
+		
+		float cgpa = 8.9f;
+		cgpa = 4.7f;
+		System.out.println(cgpa);
+		
+		String carName = "BMW";
+		carName = "Skoda";
+		System.out.println(carName);
+		
+		byte height = 7;
+		System.out.println(height);
+		
+		double velocity = 56.876543d;
+		System.out.println(velocity);
+		
+		long panNumber = 345676543l;
+		System.out.println(panNumber);
+		
+		
+	}
+	
+	
+}
