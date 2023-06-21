0ef51b549acae9cb0436963afb496a018143c478
RathnaBMohan <129740896+RathnaBMohan@users.noreply.github.com>
2023-06-21 09:26:51 +0530

    Arrays example

---
+++ Arrays.java
@@ -0,0 +1,266 @@
+public class Arrays{
+	public static void main(String[] args)
+	{
+		int[] numberOfStudents = {20, 30 , 40 ,50 , 60};
+			{
+				System.out.println(numberOfStudents[0]);
+				System.out.println(numberOfStudents[1]);
+				System.out.println(numberOfStudents[2]);
+				System.out.println(numberOfStudents[3]);
+				System.out.println(numberOfStudents[4]);
+			}
+			
+				
+				
+	    char[] gradeOfStudents = {'a' , 'b', 'c' , 'd' ,'e'};
+			{
+				System.out.println(gradeOfStudents[0]);
+				System.out.println(gradeOfStudents[1]);
+				System.out.println(gradeOfStudents[2]);
+				System.out.println(gradeOfStudents[3]);
+				System.out.println(gradeOfStudents[4]);
+			}
+			
+				
+
+		int[] ageOfStudents = {30, 45, 66, 42, 56};
+			{
+				System.out.println(ageOfStudents[0]);
+				System.out.println(ageOfStudents[1]);
+				System.out.println(ageOfStudents[2]);
+				System.out.println(ageOfStudents[3]);
+				System.out.println(ageOfStudents[4]);
+			}
+			
+				
+		String[] names = {"rathna" , "ramya" , "anju", "siddu" , "mohan" };
+			{
+				System.out.println(names[0]);
+				System.out.println(names[1]);
+				System.out.println(names[2]);
+				System.out.println(names[3]);
+				System.out.println(names[4]);
+			}
+		float[] cgpa = {6.7f , 8.2f , 8.9f , 5.4f, 7.8f};
+			{
+			System.out.println(cgpa[0]);
+			System.out.println(cgpa[1]);
+			System.out.println(cgpa[2]);
+			System.out.println(cgpa[3]);
+			System.out.println(cgpa[4]);
+			}
+			
+		long phoneNumber[] = {948155396l, 843118647l , 63662216l, 63611130l, 76858987l};
+		phoneNumber[3] = 85764345l;
+			{
+				
+				System.out.println(phoneNumber[0]);
+				System.out.println(phoneNumber[1]);
+				System.out.println(phoneNumber[2]);
+				System.out.println(phoneNumber[3]);
+				System.out.println(phoneNumber[4]);
+			}
+			
+				
+		char[] vowels = {'a' , 'e' , 'i' ,'o', 'u'};
+		vowels[2] = 'z';
+		
+			System.out.println(vowels[0]);
+		    System.out.println(vowels[1]);
+			System.out.println(vowels[2]);
+			System.out.println(vowels[3]);
+			System.out.println(vowels[4]);
+			
+				
+		int[] totalAttendance = { 25, 56, 76, 87,90};
+		totalAttendance[4]= 48;
+		{
+			System.out.println(totalAttendance[0]);
+			System.out.println(totalAttendance[1]);
+			System.out.println(totalAttendance[2]);
+			System.out.println(totalAttendance[3]);
+			System.out.println(totalAttendance[4]);
+		}
+		boolean[] isStudent = {true , false, false, true, true};
+		isStudent[1] = true;
+		{
+			System.out.println(isStudent[0]);
+			System.out.println(isStudent[1]);
+			System.out.println(isStudent[2]);
+			System.out.println(isStudent[3]);
+			System.out.println(isStudent[4]);
+		}
+		
+		byte[] heightOfPerson = {7 , 5, 6, 8, 4};
+		heightOfPerson[4] = 9;
+		{
+			System.out.println(heightOfPerson[0]);
+			System.out.println(heightOfPerson[1]);
+			System.out.println(heightOfPerson[2]);
+			System.out.println(heightOfPerson[3]);
+			System.out.println(heightOfPerson[4]);
+		}
+			
+		
+
+		int[] numberOfSubjects = new int[5];
+		{
+			 numberOfSubjects[0] = 8;
+			 numberOfSubjects[1] = 7;
+			 numberOfSubjects[2] = 10;
+			 numberOfSubjects[3] = 12;
+			 numberOfSubjects[4] = 15;
+			 
+			 {
+				 System.out.println(numberOfSubjects[0]);
+				 System.out.println(numberOfSubjects[1]);
+				 System.out.println(numberOfSubjects[2]);
+				 System.out.println(numberOfSubjects[3]);
+				 System.out.println(numberOfSubjects[4]);
+			 }
+		}
+		
+		char[] gradeOfAllStudents = new char[5];
+		{
+			gradeOfAllStudents[0] = 'a';
+			gradeOfAllStudents[1] = 'b';
+			gradeOfAllStudents[2] = 'c' ;
+			gradeOfAllStudents[3] = 'd';
+			gradeOfAllStudents[4] = 'e';
+			{
+				System.out.println(gradeOfAllStudents[0]);
+				System.out.println(gradeOfAllStudents[1]);
+				System.out.println(gradeOfAllStudents[2]);
+				System.out.println(gradeOfAllStudents[3]);
+				System.out.println(gradeOfAllStudents[4]);
+			}
+		}
+		
+		char[] allVowels = new char[5];
+		{
+            allVowels[0] = 'a';
+			allVowels[1] = 'e';
+			allVowels[2] = 'i';
+			allVowels[3] = 'o';
+			allVowels[4] = 'u';
+			{
+				System.out.println(allVowels[0]);
+				System.out.println(allVowels[1]);
+				System.out.println(allVowels[2]);
+				System.out.println(allVowels[3]);
+				System.out.println(allVowels[4]);
+			}
+		}
+		
+		byte[] id = new byte[5];
+		{
+			id[0] = 4;
+			id[1] = 6;
+			id[2] = 7;
+			id[3] = 8;
+			id[4] = 9;
+			{
+				System.out.println(id[0]);
+				System.out.println(id[1]);
+				System.out.println(id[2]);
+				System.out.println(id[3]);
+				System.out.println(id[4]);
+			}
+		}
+		
+		String[] carNames = new String[5];
+		{
+			carNames[0] = "BMW";
+			carNames[1] = "skoda";
+			carNames[2] = "benz";
+			carNames[3] = "maruthi 800";
+		    carNames[4] = "swift dzire";
+			{
+				System.out.println(carNames[0]);
+				System.out.println(carNames[1]);
+				System.out.println(carNames[2]);
+				System.out.println(carNames[3]);
+				System.out.println(carNames[4]);
+			}
+		}
+		
+
+		int[] numberOfBooks = new int[5];
+		{
+			numberOfBooks[0] = 5;
+			numberOfBooks[1] = 10;
+			numberOfBooks[2] = 15;
+			numberOfBooks[3] = 12;
+			numberOfBooks[4] = 20;
+			
+			for(int i = 0; i<=numberOfBooks.length-1;i++)
+			{
+				System.out.println(numberOfBooks[i]);
+			}
+		}
+		
+		char[] gender = new char[5];
+		{
+			gender[0] = 'f';
+			gender[1] = 'm';
+			gender[2] = 'f';
+			gender[3] = 'm';
+			gender[4] = 'm';
+			
+			for( int i=0; i<= gender.length-1; i++)
+			{
+				System.out.println(gender[i]);
+			}
+		}
+		
+		boolean[] fridayIsHoliday = new boolean[5];
+		{
+			fridayIsHoliday[0] = true;
+			fridayIsHoliday[1] = true;
+			fridayIsHoliday[2] = false;
+			fridayIsHoliday[3] = true;
+			fridayIsHoliday[4] = false;
+			
+			for(int i=0 ; i<= fridayIsHoliday.length-1; i++)
+			{
+				System.out.println(fridayIsHoliday[i]);
+			}
+		}
+		
+		String[] states = new String[5];
+		{
+			states[0] = "karnataka";
+			states[1] = "mumbai";
+			states[2] = "delhi";
+			states[3] = "kerala";
+			states[4] = "tamilnadu";
+			
+			for(int i=0;i<=states.length-1;i++)
+			{
+				System.out.println(states[i]);
+			}
+		}
+		
+		float[] percentageOfStudents = new float[5];
+		{
+			percentageOfStudents[0] = 45.6f;
+			percentageOfStudents[1] = 56.6f;
+			percentageOfStudents[2] = 98.7f;
+			percentageOfStudents[3] = 45.78f;
+			percentageOfStudents[4] = 89.6f;
+			
+			for(int i=0;i<=percentageOfStudents.length-1; i++)
+			{
+				System.out.println(percentageOfStudents[i]);
+			}
+		}
+		
+			
+			
+			
+
+
+	}
+}
+
+	
